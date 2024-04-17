fd59e53be3de961a4c86b0e9519a64a8ec7909aa
marksobolev <mark.sobolev@gotech.io>
2024-04-17 19:22:36 +0300

    Clean a bit code

--- src/pages/DroneListPage/utils.ts
+++ src/pages/DroneListPage/utils.ts
@@ -3,12 +3,15 @@ import { DroneCardEntity } from '../../api/drones/types';
 
 
 export const getDronesFromLocalStorage = () => {
-  const userData = localStorage.getItem(STORAGE_KEY);
-  return (userData ? JSON.parse(userData) : []) as DroneCardEntity[];
+  return getValueFromLocalStorage<DroneCardEntity[]>(STORAGE_KEY, []);
 };
 
 export const getDroneImageFromLocalStorage = (id: string) => {
-  const userData = localStorage.getItem(IMAGE_STORAGE_KEY);
-  return ((userData ? JSON.parse(userData) : {}) as Record<string, string>)[id];
+  const userData = getValueFromLocalStorage<Record<string, string>>(IMAGE_STORAGE_KEY, {});
+  return userData[id];
 };
 
+const getValueFromLocalStorage = <T extends any>(id: string, safetyData: T) => {
+  const data = localStorage.getItem(id);
+  return (data ? JSON.parse(data) : safetyData) as T;
+};
